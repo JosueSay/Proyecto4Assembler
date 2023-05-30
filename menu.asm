90863000cd17c1a9f72fa963b0988ccd4fd45982
JosueSay <106031855+JosueSay@users.noreply.github.com>
2023-05-30 10:23:50 -0600

    Update menu.asm

--- menu.asm
+++ menu.asm
@@ -17,16 +17,23 @@
 	;======================================
 	; || Variables para mostrar opciones ||
 	;======================================
-	msg_bienvenida BYTE "Bienvenido al juego de ranas. Escoge la opcion que desees:", 0
+	msg_bienvenida BYTE "Bienvenido al juego de ranas. Escoge la opcion que desees:",0Ah ,0
+	msg_opcion1 BYTE "Opcion 1. Ingresar al juego", 0Ah, 0
+	msg_opcion2 BYTE "Opcion 2. Brindar instrucciones",0Ah ,0
+	msg_opcion3 BYTE "Opcion 3. Salir del juego", 0Ah, 0
 	respuesta_menu BYTE 0
-	msg_opcion1 BYTE "Opcion 1. Ingresar al juego", 0
-	msg_opcion2 BYTE "Opcion 2. Brindar instrucciones", 0
-	msg_opcion3 BYTE "Opcion 3. Salir del juego", 0
-	
+
 	;==========================================
 	;|| Variables para mostrar instrucciones ||
 	;==========================================
-	msg_instrucciones BYTE "El juego consta de mover 3 ranas del mismo color al lado contrario de su posición inicial.\n Las ranas del color AZUL son 'a', 'b' y 'c' y las ranas de color ROJO son 'd', 'e', 'f'.\nToma en cuenta lo siguiente:\n\ti.	Las ranas no pueden retroceder\n\tii.	Solo pueden moverse un espacio a la vez.\n\tiii.	Las ranas de distinta especie pueden saltarse otra rana si existe un espacio vació adelante.\n\tiv.	iv.	Sin embargo, las ranas de la misma especie no pueden saltarse entre ellas.", 0
+	msg_instrucciones1 BYTE "El juego consta de mover 3 ranas del mismo color al lado contrario de su posición inicial.", 0
+	msg_instrucciones2 BYTE "Las ranas del color AZUL son 'a', 'b' y 'c' y las ranas de color ROJO son 'd', 'e', 'f'.", 0
+	msg_instrucciones3 BYTE "Toma en cuenta lo siguiente:", 0
+	msg_instrucciones4 BYTE "\ti.	Las ranas no pueden retroceder", 0
+	msg_instrucciones5 BYTE "\tii.	Solo pueden moverse un espacio a la vez.", 0
+	msg_instrucciones6 BYTE "\tiii.	Las ranas de distinta especie pueden saltarse otra rana si existe un espacio vació adelante.", 0
+	msg_instrucciones7 BYTE "\tiv.	Sin embargo, las ranas de la misma especie no pueden saltarse entre ellas.", 0
+
 	msg_estadoI1 BYTE "======================================",0
 	msg_estadoI2 BYTE "|| a || b || c ||---|| d || e || f ||",0
 	msg_estadoI3 BYTE "======================================",0
@@ -89,7 +96,11 @@ main proc
 	cmp BYTE PTR [respuesta_menu], 2
 	jne finalizar
 
-	push OFFSET msg_instrucciones
+		push OFFSET msg_instrucciones1
+	call printf
+	add esp, 4
+	
+	push OFFSET msg_instrucciones2
 	call printf
 	add esp, 4
 
